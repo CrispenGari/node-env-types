bd4830ae7253f8159e9ca0ce9f01fdcc64d97191
CrispenGari <Crispengari@gmail.com>
2022-07-18 18:04:32 +0200

    --

---
+++ lib/src/utils/index.d.ts
@@ -0,0 +1,6 @@
+export declare const commentRegEx: RegExp;
+export declare const isComment: (line: string) => boolean;
+export declare const startWithExport: (line: string) => boolean;
+export declare const isValidVariableName: (variable: string) => boolean;
+export declare const isMultiLineVar: (line: string) => boolean;
+//# sourceMappingURL=index.d.ts.map
