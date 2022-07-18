92230404b39a145b32ba8a52954bc42cdba11b9b
CrispenGari <Crispengari@gmail.com>
2022-07-18 17:41:56 +0200

    bug

--- lib/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Options } from "./types";
-declare const createEnvTypes: (rootPath?: string, options?: Options) => void;
-export default createEnvTypes;
-//# sourceMappingURL=index.d.ts.map
